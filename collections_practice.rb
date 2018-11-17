baeea6fd4695e394a94c54189dada8a1b2696201
Arnold Su <arnoldtingsu@gmail.com>
2018-11-17 13:34:14 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,27 @@
 
+def sort_array_asc(list)
+  list.sort
+end
+
+def sort_array_desc(list)
+  list.sort.reverse
+end
+
+def sort_array_char_count(list)
+  list.sort_by {|x| x.length}
+end
+  
+def swap_elements(list)
+  list[1], list[2] = list[2], list[1] 
+  list
+end
+
+def reverse_array(list)
+  list.reverse
+end
+
+def kesha_maker(list)
+  list.each do |letter|
+    letter[2] = "$"
+  end
+end  
